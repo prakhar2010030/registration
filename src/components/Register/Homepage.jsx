d9632bd48b6d258503b75663b7c24ddabba1bbab
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-04-28 01:32:59 +0530

    responsive homepage

--- src/components/Register/Homepage.jsx
+++ src/components/Register/Homepage.jsx
@@ -5,25 +5,25 @@ const Homepage = () => {
     <>
     <div className='cont'>
 
-    <div className=' .image_home_left'>
+    <div className='image_left'>
 
     </div>
-    <div className=' .image_home_right'>
+    <div className='image_right'>
       <div className='text'>
         <h1>
           Hooray!
         </h1>
 
       </div>
-        <h2 className='.heading_home'>
+        <h2 className='heading_home'>
           <br></br>
           You've Registered Successfully.
         </h2>
       <div className='image3'>
       </div>  
-        {/* <div className='image4'>
+        {/* { <div className='image4'>
 
-        </div> */}
+        </div> } */}
     </div>
     </div>
 
