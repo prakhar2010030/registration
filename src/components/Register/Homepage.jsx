b532c582506409c56bed05786b8d5db4f80da383
shubhi182001 <kediashubhi@gmail.com>
2022-05-01 14:50:16 +0530

    refresh

--- src/components/Register/Homepage.jsx
+++ src/components/Register/Homepage.jsx
@@ -1,6 +1,6 @@
-import React ,{ useEffect, useState } from "react";
+import React  from "react";
 import "./Homepage.css";
-import {  useNavigate } from "react-router-dom";
+// import {  useNavigate } from "react-router-dom";
 const Homepage = () => {
   
 
